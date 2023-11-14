43d8113a65746d698a08b1697f5f477569e8f769
Michael Clear <clearm@tcd.ie>
2023-11-14 08:33:53 +0000

    Finished outline of parser

--- parser.php
+++ parser.php
@@ -1,5 +1,6 @@
 <?php
 
+include("commmon.php");
 
 class Term {
     public const EPSILON = "EPSILON";
@@ -49,8 +50,10 @@ class Production {
 class Grammar {
     private $table;
     private $terminal_names;
+    private $start_nonterminal;
 
-    public function __construct($terminal_names, $table) {
+    public function __construct($start_nonterminal, $terminal_names, $table) {
+        $this->start_nonterminal = $start_nonterminal;
         $this->terminal_names = $terminal_names;
         $this->table = $table;
     }
@@ -58,15 +61,37 @@ class Grammar {
     public function has_terminal($name) {
         return in_array($name, $terminal_names);
     }
+
+    public function starting_nonterminal() {
+        return $this->start_nonterminal;
+    }
+
+    public function has_production($nonterminal, $head_terminal) {
+        assert(in_array($nonterminal, $this->table),
+               "No productions for nonterminal $nonterminal");
+        $rules = $this->table[$nonterminal];
+        return in_array($head_terminal, $rules);
+    }
+
+
+    public function get_production($nonterminal, $head_terminal) {
+        assert($this->has_production($nonterminal, $head_terminal),
+               "Production for head terminal $head_terminal does not exist for nonterminal $nonterminal");
+        $rules = $this->table[$nonterminal];
+        return $rules[$head_terminal];
+    }
+           
 }
 
 class GrammarBuilder {
     private $table;
     private $terminal_names;
+    private $start_nonterminal;
 
-    public function __construct($terminal_names) {
+    public function __construct($start_nonterminal, $terminal_names) {
         $this->table = array();
         $this->terminal_names = $terminal_names;
+        $this->start_nonterminal = $start_nonterminal;
     }
 
 
@@ -75,7 +100,7 @@ class GrammarBuilder {
               "Expected first term to be a terminal or the terms to be empty");
        $prod = new Production($nonterminal_name, $terms);
 
-       $sel_terminal = Term.EPSILON;
+       $sel_terminal = Term::EPSILON;
        if (count($terms) > 0) {
            $sel_terminal = $terms[0]->name();
            assert(in_array($sel_terminal, $this->terminal_names), "Head terminal in production is not a valid terminal");
@@ -96,10 +121,95 @@ class GrammarBuilder {
 
 
     public function get_grammar() {
-        return new Grammar($terminal_names, $table);
+        assert(in_array($this->start_nonterminal, $this->table),
+               "No productions for starting nonterminal in grammar");
+        return new Grammar($this->start_nonterminal, $this->terminal_names,
+                           $this->table);
     }
 }
 
+
+class Parser {
+    private $grammar;
+    private $id_counter;
+
+    public function __construct($grammar) {
+        $this->grammar = $grammar;
+        $this->id_counter = 0;
+    }
+
+
+    public function parse($tokens) {
+        $valid = true;
+        $id = $id_counter++;
+        $position = 0;
+        $len = count($tokens);
+        $grammar = $this->grammar;
+
+        $stack = array();
+        array_push($stack, $grammar->starting_nonterminal());
+
+        // notify listeners of begin with id
+        while (($term = array_pop($stack)) != NULL) {
+
+            if ($position >= $len) {
+                if ($term->is_terminal ||
+                        !$grammar->has_production($term->name,
+                                                  Term::EPSILON)) {
+                    // notify listeners of syntax error: Expected epsilon
+                    $valid = false;
+                    break;
+                }
+                else {
+                    continue;
+                }
+            }
+
+            $head_token = $tokens[$position];
+            if ($term->is_terminal) {
+               if ($term->name == $head_token->type) {
+                   // Correct match: consume - notify listeners of consume
+                   $position++;
+
+               }
+               else {
+                    // Mismatch (error) - notify listeners of syntax error
+                    $valid = false;
+                    break;
+              }
+            }
+            else {
+                // So the term we've popped is a nonterminal
+                // we need to push the terms in the right production
+                // onto the stack. The right production depends on head token
+                $nonterminal = $term->name;
+                if ($grammar->has_production($nonterminal, $head_token->type)) {
+                    // Push terms in production onto the stack
+                    $prod = $grammar->get_production($nonterminal,
+                                                     $head_token->type);
+                    $terms = $prod->terms();
+                    while (($t = array_pop($terms)) != NULL) {
+                        array_push($stack, $t);
+                    }
+
+                }
+                else if ($grammar->has_production($nonterminal, Term::EPSILON)) {
+                    // If the nonterminal has the empty production, continue
+                    continue;
+                }
+                else {
+                     // Syntax error - notify listeners of error
+                     $valid = false;
+                     break;
+                }
+            }
+            
+        }
+        
+
+        return $valid;
+    }
+}
 
 
 
