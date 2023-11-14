82e79fec3cf7ca245304795195990af77372af4c
Michael Clear <clearm@tcd.ie>
2023-11-14 08:32:04 +0000

    Added skeleton common.php and scanner.php

---
+++ common.php
@@ -0,0 +1,12 @@
+<?php
+
+class Token {
+      private $type;
+      private $content;
+
+      public function __construct($type, $content=NULL) {
+             $this->type = type;
+             $this->content = content;
+      }
+}
+
